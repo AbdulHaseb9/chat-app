e9f9def6cc1835bff3fe064064ec5e4fa78ca69c
AbdulHaseb9 <haseebsheikh2380@gmail.com>
2024-11-12 02:22:06 +0500

    set up project install  tailwindcss

--- server/index.js
+++ server/index.js
@@ -1,15 +1,33 @@
 import express from "express"
 import dotenv from "dotenv"
+import { Server } from "socket.io";
+import { createServer } from "node:http"
+import cors from "cors"
 
 dotenv.config()
+const port = process.env.PORT || 8000
 
 const app = express()
-const port = process.env.PORT || 8000
+const server = createServer(app)
+
+const io = new Server(server, {
+    cors: {
+        origin: "*",
+        methods: ["GET", "POST"],
+        credentials: true
+    }
+})
+
+io.on("connection", (socket) => {
+    console.log("user connected");
+    console.log("id", socket.id);
+})
+
 
 app.get("/", (req, res) => {
     res.send("Hello, World!");
 });
 
-app.listen(port, () => {
+server.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
 });
