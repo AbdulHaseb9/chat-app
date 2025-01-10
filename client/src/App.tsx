ab4e97d1b168059439c0a4a81e6279a2c1769a7f
AbdulHaseb9 <haseebsheikh2380@gmail.com>
2025-01-10 20:38:41 +0500

    create connection between server an client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,23 @@
 import "./App.css";
+import io from "socket.io-client";
+import { useEffect, useMemo } from "react";
 
 function App() {
+  const socket = useMemo(() => {
+    return io("http://localhost:8000");
+  }, []);
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log(socket.id);
+    });
+  }, []);
+
   return (
     <>
-      <h1 className="text-3xl text-center my-6 text-green-700">Chat Application</h1>
+      <h1 className="text-3xl text-center my-6 text-green-700">
+        Chat Application
+      </h1>
     </>
   );
 }
--- server/index.js
+++ server/index.js
@@ -1,28 +1,38 @@
 import express from "express"
 import dotenv from "dotenv"
-import { Server } from "socket.io";
-import { createServer } from "node:http"
+import http from "http"
 import cors from "cors"
+import { Server } from "socket.io";
 
 dotenv.config()
 const port = process.env.PORT || 8000
 
 const app = express()
-const server = createServer(app)
+
+app.use(express.json())
+app.use(cors({
+    origin: "http://localhost:5173",
+    methods: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true
+}))
+
+const server = http.createServer(app)
 
 const io = new Server(server, {
     cors: {
-        origin: "*",
+        origin: "http://localhost:5173",
         methods: ["GET", "POST"],
         credentials: true
     }
-})
+});
 
 io.on("connection", (socket) => {
     console.log("user connected");
     console.log("id", socket.id);
+    socket.on("disconnect", () => {
+        console.log(`user disconnected ${socket.id}`);
+    })
 })
-
 
 app.get("/", (req, res) => {
     res.send("Hello, World!");
